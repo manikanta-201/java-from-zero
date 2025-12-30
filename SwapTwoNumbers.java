68cbda59d03876cf5895b08520dd42448a890d6b
manikanta <manikant66677@gmail.com>
2025-12-30 16:04:39 +0530

    first commit

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,17 @@
+
+public class SwapTwoNumbers{
+public static void main(String[] args){
+int m = 9, n =5;
+int temp =m;
+m=n;
+n=temp;
+System.out.println(" value of m =" + m + " value of n = "+ n);
+}
+
+}
+
+
+
+
+
+
