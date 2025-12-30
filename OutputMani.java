a5ef24cbe3143e7583ccfd87bcc10cfa039f7a95
manikanta <manikant66677@gmail.com>
2025-12-30 16:51:59 +0530

    Add OutputMani.java

---
+++ OutputMani.java
@@ -0,0 +1,14 @@
+public class OutputMani{
+public static void main(String[] args){
+int x = 20, y = 30;
+char z = 'T';
+String s = "Manikanta";
+
+System.out.println(x);
+System.out.println(x + y);
+System.out.println(x + " " + y);
+System.out.print(z + " ");
+System.out.println(s);
+System.out.println("Well com to java programming");
+}
+}
