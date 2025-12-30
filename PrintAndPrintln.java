806d4543f2844d780ccbb9e427d0f06c340632bc
manikanta <manikant66677@gmail.com>
2025-12-30 16:41:41 +0530

    Add  PrintAndPrintln.java

---
+++ PrintAndPrintln.java
@@ -0,0 +1,15 @@
+public class PrintAndPrintln{
+public static void main(String[] args){
+int x = 10, y = 20;
+char z = 'G';
+String str = "GFG";
+
+System.out.println(x);
+System.out.println(x+y);
+System.out.println(x +" " +y);
+System.out.print(str + " ");
+System.out.print("courses\n");
+System.out.print("manikanta he want to becom good java developer");
+
+}
+}
