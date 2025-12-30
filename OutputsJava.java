88eaebbfa2261295180a6bad25bc2af8588cf783
manikanta <manikant66677@gmail.com>
2025-12-30 16:43:58 +0530

    Add OutputsJava.java

---
+++ OutputsJava.java
@@ -0,0 +1,15 @@
+public class OutputsJava{
+public static void main(String[] args){
+int a= 100,b=500;
+float z =(float) Math.PI;
+System.out.println(z);
+
+System.out.format("valu of PI=%2f\n",z);
+System.out.format("valu of PI=%5.2f\n",z);
+System.out.format("valu of PI=%05.2f\n",z);
+
+
+System.out.printf(" a= %d, b= %d", a, b);
+
+}
+}
