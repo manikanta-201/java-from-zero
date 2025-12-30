b3908af760eda35bac467bda2bdbf39e3f21444c
manikanta <manikant66677@gmail.com>
2025-12-30 18:01:08 +0530

    Add  GfG.java

---
+++ GfG.java
@@ -0,0 +1,12 @@
+public class GfG{
+ public static void main(String[] args){
+double d = 100.04;
+long num=(long)d;
+int i=(int)num;
+
+System.out.println("Double value ="+ d);
+System.out.println("long value ="+ num);
+System.out.println("int value ="+ i);
+
+}
+}
